cdb9ec17934bbdaf9326082420bd52888a6e1e91
Robin Westerhoff <rwesterhoff@me.com>
2016-09-30 23:28:47 +0200

    Hiding notifications, cleaning up code.

--- js/app.js
+++ js/app.js
@@ -35,6 +35,7 @@ function Alert(text) {
 Alert.prototype.toHTML = function() {
     var controlsHTML = '<div';
     controlsHTML += ' class="alert"';
+    controlsHTML += ' data-state="visible"';
     controlsHTML += '>';
     controlsHTML += '<p><strong>Alert</strong>' + this.text + '</p>';
     controlsHTML += '<button class="alert-button-close">Close alert</button>';
@@ -42,35 +43,12 @@ Alert.prototype.toHTML = function() {
     return controlsHTML;
 };
 
-function activateElement(selector) {
-    var elementList = document.getElementsByClassName(selector);
-
-    function makeActive() {
-        makeOthersInactive();
-        this.classList.add("active");
-    }
-
-    function makeOthersInactive() {
-        for (var i = 0; i < elementList.length; i++) {
-            elementList[i].classList.remove("active");
-        }
-    }
-
-    for (var i = 0; i < elementList.length; i++) {
-        elementList[i].addEventListener("click", makeActive);
-    }
-}
-
 /* ====================================================================================== *\
     DASHBOARD
 *\ ====================================================================================== */
 function Dashboard() {
     var alertsNotification = document.getElementById('app-notification');
-
-    this.displayAll = function() {
-        this.checkAlerts(alerts);
-    };
-    this.checkAlerts = function(obj) {
+    this.setAlerts = function(obj) {
         amount = this.checkAmount(obj);
         if (amount === 1) {
             text = 'Notification';
@@ -78,13 +56,12 @@ function Dashboard() {
             text = 'Notifications';
         }
         if (this.isEmpty(obj)) {
-            this.hideAlerts();
+            this.hideNotifications();
         } else {
+            this.showNotifications();
             this.displayAlerts();
         }
         alertsNotification.innerHTML = '<p>' + amount + ' ' + text + '</p>';
-        // If x button is clicked
-        // Delete alert
     };
     this.displayAlerts = function() {
         var alertsContainer = new Container('content-notifications', 'visible'),
@@ -93,16 +70,55 @@ function Dashboard() {
         var container = alertsContainer.id,
             html = alerts;
         this.renderInElement(container, html);
+        this.setCloseButton();
     };
-    this.hideAlerts = function() {
-        alertsNotification.classList.remove('active');
+    this.showNotifications = function() {
+        alertsNotification.setAttribute('data-state', 'active');
+    };
+    this.hideNotifications = function() {
+        alertsNotification.setAttribute('data-state', 'inactive');
+    };
+    this.setCloseButton = function() {
+        var removeAlertsButtons = document.getElementsByClassName('alert-button-close'),
+            removeAlert = function() {
+                var elementToRemove = this.parentNode;
+                elementToRemove.setAttribute('data-state', 'hidden');
+                setTimeout(function() {
+                    elementToRemove.parentNode.removeChild(elementToRemove)
+                }, 200);
+                amount -= 1;
+                if (amount === 0) {
+                    dashboard.hideNotifications();
+                }
+            };
+        for (var i = 0; i < removeAlertsButtons.length; i++) {
+            removeAlertsButtons[i].addEventListener("click", removeAlert);
+        }
+    };
+    this.displayAll = function() {
+        this.setStates("nav-button");
+        this.setAlerts(alerts);
     };
 }
+Dashboard.prototype.setStates = function(selector) {
+    var elementList = document.getElementsByClassName(selector),
+        setToActive = function() {
+            setToInactive();
+            this.setAttribute('data-state', 'active');
+        },
+        setToInactive = function() {
+            for (var i = 0; i < elementList.length; i++) {
+                elementList[i].setAttribute('data-state', 'inactive');
+            }
+        };
+    for (var i = 0; i < elementList.length; i++) {
+        elementList[i].addEventListener("click", setToActive);
+    }
+};
 Dashboard.prototype.renderInElement = function(container, html) {
     var content = html,
         element = document.getElementById(container);
     for (var prop in content) {
-
         if (content.hasOwnProperty(prop)) {
             element.innerHTML += content[prop].toHTML();
         }
@@ -110,8 +126,7 @@ Dashboard.prototype.renderInElement = function(container, html) {
 };
 Dashboard.prototype.checkAmount = function(obj) {
     var count = 0;
-    var i;
-    for (i in obj) {
+    for (var i in obj) {
         if (obj.hasOwnProperty(i)) {
             count++;
         }
@@ -120,8 +135,9 @@ Dashboard.prototype.checkAmount = function(obj) {
 };
 Dashboard.prototype.isEmpty = function(obj) {
     for (var prop in obj) {
-        if (obj.hasOwnProperty(prop))
+        if (obj.hasOwnProperty(prop)) {
             return false;
+        }
     }
     return true;
 };
@@ -135,5 +151,4 @@ var dashboard = new Dashboard(),
         alert_2: new Alert("Maecenas faucibus mollis interdum.", 'visible'),
         alert_3: new Alert("Consectetur Ipsum.", 'visible')
     };
-activateElement("nav-button");
 dashboard.displayAll();
